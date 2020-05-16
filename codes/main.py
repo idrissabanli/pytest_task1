33e88538aa09ff8b5dbaa2f4d813827d60070db0
idris <idris.sabanli@gmail.com>
2020-05-17 00:54:28 +0400

    initial commit

---
+++ codes/main.py
@@ -0,0 +1,6 @@
+
+
+def divide(arg1, arg2):
+    if arg2 == 0:
+        return f'You cannot divide 0!'
+    return arg1/arg2
---
+++ tests/test_code.py
@@ -0,0 +1,19 @@
+from codes.main import divide
+
+def test_divide1():
+    '''
+    given: arg1=3, arg2=2, expected = 1.5
+    '''
+    pass
+
+def test_divide2():
+    '''
+    given: arg1=4, arg2=2, expected = 2.0
+    '''
+    pass
+
+def test_divide3():
+    '''
+    given: arg1=10, arg2=0, expected = 'You cannot divide 0!'
+    '''
+    pass
